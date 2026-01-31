d7596b6aa5cb5ebeb573edbd2cefe11e9b903da0
Andrew Udvare <audvare@gmail.com>
2026-01-31 00:03:48 -0500

    main: unblock from other OS

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,8 @@
 
 #include "browserchooser.h"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
 int main(int argc, char *argv[]) {
-#pragma clang diagnostic pop
     QSettings::setDefaultFormat(QSettings::IniFormat);
-#ifndef Q_OS_LINUX
-    QApplication app(argc, argv);
-    QMessageBox::critical(nullptr,
-                          QObject::tr("Browser Chooser"),
-                          QObject::tr("This application only supports Linux."));
-    return 1;
-#else
     if (argc < 2) {
         qCritical("Usage: %s <url>", argv[0]);
         return 1;
@@ -23,5 +13,4 @@ int main(int argc, char *argv[]) {
     auto urlToOpen = QString::fromUtf8(argv[1]);
     BrowserChooser chooser(urlToOpen);
     return chooser.exec();
-#endif
 }
